f90189531f00deec5a54b4b90eaab443cd34022b
mendri <marcalmendrighi@gmail.com>
2023-09-05 10:05:05 -0300

    feat: estilizando um pouco

--- Frontend/src/Components/CategoryFilter.tsx
+++ Frontend/src/Components/CategoryFilter.tsx
@@ -5,8 +5,8 @@ function CategoryFilter() {
     const { searchProducts } = useContext(ProductsContext);
 
     return (
-        <select onChange={(e) => searchProducts(e.target.value)} data-testid="category-dropdown-testid" className="w-2/6 text-white font-extrabold bg-blue-950 p-2 rounded-md shadow-md">
-            <option value="">All Categories</option>
+        <select onChange={(e) => searchProducts(e.target.value)} data-testid="category-dropdown-testid" className="w-5/12 text-white font-extrabold bg-blue-950 p-2 rounded-md shadow-md">
+            <option value="">Escolha uma categoria:</option>
             <option value="celular">Mobile</option>
             <option value="smart tv">Tv</option>
             <option value="geladeira">Refrigerator</option>
