91c3d16cf7e115411c8b96fd3d19b676b4436b10
mendri <marcalmendrighi@gmail.com>
2023-09-05 22:08:36 -0300

    feat: feito o deploy, atualizações de interface

--- Scraper/routes/web.php
+++ Scraper/routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+  return view('welcome');
+});
+
+
 Route::get('/scraper', 'App\Http\Controllers\ScraperController@index');
